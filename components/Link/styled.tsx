7dfa289669ade5380508b68e36ab4bf7a3989f73
Peter Simone <plssimone@gmail.com>
2023-12-21 22:51:33 -0500

    add disabling to link

--- components/Link/styled.tsx
+++ components/Link/styled.tsx
@@ -1,17 +1,18 @@
 import Link from "next/link";
 import styled, { css } from "styled-components";
 
-const LinkStyle = css`
+const LinkStyle = css<{ $disabled?: string }>`
   appearance: none;
   color: inherit;
   text-decoration: none;
   font-family: inherit !important;
-  cursor: pointer;
 
   &:focus-visible {
     outline: 1px solid ${({ theme }): string => theme.greyBorder};
     border-radius: 10px;
   }
+  cursor: ${({ $disabled }): string => ($disabled ? "default" : "pointer")};
+  pointer-events: ${({ $disabled }): string => ($disabled ? "none" : "all")};
 `;
 
 export const StyledNextLink = styled(Link)`
