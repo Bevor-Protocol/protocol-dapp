44c81c86df3cb326192662f3958d7a13e68d3f43
Peter Simone <plssimone@gmail.com>
2023-12-22 13:38:47 -0500

    add some dynamic rendering based on owner

--- app/user/[slug]/page.tsx
+++ app/user/[slug]/page.tsx
@@ -1,9 +1,24 @@
-import { Section } from "@/components/Common";
+"use client";
+import { usePathname } from "next/navigation";
+
+import { Section, Loader } from "@/components/Common";
+import { useIsMounted } from "@/hooks/useIsMounted";
+import { useAccount } from "wagmi";
 
 export default (): JSX.Element => {
+  const pathname = usePathname();
+  const mounted = useIsMounted();
+  const { address } = useAccount();
+
+  const routeAddress = pathname.substring(pathname.lastIndexOf("/") + 1);
+
+  const isOwner = address == routeAddress;
+
   return (
     <Section $padCommon $centerV $centerH>
-      <h2>Hey there</h2>
+      {!mounted && <Loader $size="50px" />}
+      {mounted && isOwner && <h2>You own this address, you can see everything.</h2>}
+      {mounted && !isOwner && <h2>You don't own this address, what you see is limited.</h2>}
     </Section>
   );
 };
