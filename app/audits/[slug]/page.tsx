093936cadc5942fabecf0be23208cb75cebdc982
Peter Simone <plssimone@gmail.com>
2023-12-22 15:12:59 -0500

    convert audit page to SSR from local file

--- app/audits/[slug]/page.tsx
+++ app/audits/[slug]/page.tsx
@@ -1,5 +1,4 @@
 import AuditDashboard from "@/components/pages/AuditDashboard";
-import { mockAuditInfo } from "@/utils/constants";
 
 import fs from "fs";
 import path from "path";
@@ -11,38 +10,6 @@ import matter from "gray-matter";
 import { Section } from "@/components/Common";
 import { H2 } from "@/components/Text";
 import { AuditSection } from "@/components/pages/Audits";
-import { Address } from "wagmi";
-import { AuditDashI } from "@/utils/types";
-
-const getData = (): AuditDashI => {
-  const auditor = mockAuditInfo.auditors[0] as Address;
-  const auditee = mockAuditInfo.auditee as Address;
-  const cliff = mockAuditInfo.cliff;
-  const start = mockAuditInfo.start;
-  const duration = mockAuditInfo.duration;
-  const slicePeriodSeconds = mockAuditInfo.slicePeriodSeconds;
-  const withdrawlPaused = mockAuditInfo.withdrawlPaused;
-  const amountTotal = mockAuditInfo.amountTotal;
-  const withdrawn = mockAuditInfo.withdrawn;
-  const auditInvalidated = mockAuditInfo.auditInvalidated;
-  const token = mockAuditInfo.token as Address;
-  const tokenId = mockAuditInfo.tokenId;
-
-  return {
-    auditor,
-    auditee,
-    cliff,
-    start,
-    duration,
-    slicePeriodSeconds,
-    withdrawlPaused,
-    amountTotal,
-    withdrawn,
-    auditInvalidated,
-    token,
-    tokenId,
-  };
-};
 
 type MarkdownI = {
   data: {
@@ -51,8 +18,11 @@ type MarkdownI = {
   content: string;
 };
 
-const getMarkdown = async (): Promise<MarkdownI> => {
-  const fullPath = path.join("./public", "TEST.md");
+const getMarkdown = async (display: string): Promise<MarkdownI> => {
+  if (!["details", "audit"].includes(display)) {
+    display = "details";
+  }
+  const fullPath = path.join("./public", `${display}.md`);
   const fileContents = fs.readFileSync(fullPath, "utf8");
 
   const { data, content } = matter(fileContents);
@@ -64,14 +34,20 @@ const getMarkdown = async (): Promise<MarkdownI> => {
   };
 };
 
-export default async ({ params }: { params: { slug: string } }): Promise<JSX.Element> => {
-  const audit = await getData();
-  await getMarkdown();
+export default async ({
+  params,
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams: { [key: string]: string | undefined };
+}): Promise<JSX.Element> => {
+  const display = searchParams.display ?? "details";
+  const { content, data } = await getMarkdown(display);
   return (
     <Section $fillHeight $padCommon $centerH $centerV>
       <AuditSection>
         <H2>Audit Dashboard {params.slug}</H2>
-        <AuditDashboard audit={audit} />
+        <AuditDashboard data={data} content={content} display={display} />
       </AuditSection>
     </Section>
   );
