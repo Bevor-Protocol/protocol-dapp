4a5c0b3a97a43e7a63349e1253d0479b7531a598
Peter Simone <plssimone@gmail.com>
2024-02-23 17:09:45 -0500

    update logo svgs to tsx, add icons to wallet modal

--- components/Web3/wallets.tsx
+++ components/Web3/wallets.tsx
@@ -1,8 +1,22 @@
+/* eslint-disable @next/next/no-img-element */
+import React, { useState, useEffect } from "react";
 import { useConnect } from "wagmi";
+import { config } from "@/providers/wallet/config";
 
 import { useModal } from "@/hooks/contexts";
+import { NavItem } from "@/components/Nav/styled";
+import { Tag } from "@/components/Common";
+import { Span } from "@/components/Text";
+import { sortWallets } from "@/lib/utils";
+import { CoinbaseWallet, WalletConnect } from "@/assets/wallets";
+
+const IconMapper: Record<string, React.ReactNode> = {
+  walletConnect: <WalletConnect height="20" width="20" />,
+  coinbaseWalletSDK: <CoinbaseWallet height="20" width="20" />,
+};
 
 const Wallets = (): JSX.Element => {
+  const [recentConnector, setRecentConnector] = useState("");
   const { toggleOpen } = useModal();
   const { connect, connectors } = useConnect({
     mutation: {
@@ -11,12 +25,46 @@ const Wallets = (): JSX.Element => {
     },
   });
 
+  useEffect(() => {
+    const getRecent = async (): Promise<void> => {
+      const recentPromise = config.storage?.getItem("recentConnectorId") || "";
+      if (recentPromise) {
+        setRecentConnector((await Promise.resolve(recentPromise)) || "");
+      }
+    };
+    getRecent();
+  }, []);
+
+  const walletsShow = sortWallets([...connectors], recentConnector, true);
+
   return (
     <div>
-      {connectors.map((connector) => (
-        <div key={connector.uid}>
-          <button onClick={(): void => connect({ connector })}>{connector.name}</button>
-        </div>
+      {walletsShow.map((connector) => (
+        <NavItem
+          key={connector.uid}
+          onClick={(): void => connect({ connector })}
+          $height="fit-content"
+          $align="center"
+          $justify="flex-start"
+          $gap="md"
+          $active={true}
+          $padding="5px 10px"
+          style={{ cursor: "pointer" }}
+        >
+          <div style={{ height: "20px", width: "20px" }}>
+            {connector.icon ? (
+              <img
+                src={connector.icon}
+                alt="wallet logo"
+                style={{ height: "100%", width: "100%" }}
+              />
+            ) : (
+              IconMapper[connector.id]
+            )}
+          </div>
+          <Span style={{ whiteSpace: "nowrap" }}>{connector.name}</Span>
+          {recentConnector == connector.id && <Tag>recent</Tag>}
+        </NavItem>
       ))}
     </div>
   );
--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,5 +1,26 @@
 import { Address } from "viem";
+import { Connector } from "wagmi";
 
 export const trimAddress = (address: Address | undefined): string => {
   return address?.substring(0, 6) + "..." + address?.substring(address.length - 3, address.length);
+};
+
+export const sortWallets = (
+  array: Connector[],
+  recent: string,
+  excludeInjected = true,
+): Connector[] => {
+  const arraySorted = array.toSorted((a, b) => a.name.localeCompare(b.name));
+  if (excludeInjected) {
+    const injectedIndex = arraySorted.findIndex((item) => item.id === "injected");
+    arraySorted.splice(injectedIndex, 1);
+  }
+  const recentIndex = arraySorted.findIndex((item) => item.id === recent);
+
+  if (recentIndex !== -1) {
+    const recentItem = arraySorted.splice(recentIndex, 1);
+    arraySorted.unshift(...recentItem);
+  }
+
+  return arraySorted;
 };
