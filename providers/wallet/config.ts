b706b65d89636d47990faabc8e36ab1c55c15b57
Peter Simone <plssimone@gmail.com>
2024-02-23 17:09:09 -0500

    only include injected provider for now

--- providers/wallet/config.ts
+++ providers/wallet/config.ts
@@ -1,17 +1,19 @@
 import { http, cookieStorage, createStorage, createConfig } from "wagmi";
-import { walletConnect, injected, coinbaseWallet } from "wagmi/connectors";
+// import { walletConnect, injected, coinbaseWallet } from "wagmi/connectors";
+import { injected } from "wagmi/connectors";
+
 import { goerli, polygonMumbai } from "wagmi/chains";
 
 const projectId = process.env.NEXT_PUBLIC_WC_PROJECT_ID as string;
 
 if (!projectId) throw new Error("Project ID is not defined");
 
-const metadata = {
-  name: "Bevor",
-  description: "Bevor dApp",
-  url: "https://app.bevor.io",
-  icons: ["https://www.bevor.io/apple-icon.png"],
-};
+// const metadata = {
+//   name: "Bevor",
+//   description: "Bevor dApp",
+//   url: "https://app.bevor.io",
+//   icons: ["https://www.bevor.io/apple-icon.png"],
+// };
 
 // Rather than using the web3modal defaultWagmiConfig(), I elect to use the custom
 // wagmi createConfig(). We lose some metadata, description and url become unused,
@@ -28,13 +30,13 @@ const config = createConfig({
     [polygonMumbai.id]: http(),
   },
   connectors: [
-    walletConnect({ projectId, metadata, showQrModal: false }),
+    // walletConnect({ projectId, metadata, showQrModal: false }),
     injected({ shimDisconnect: true }),
-    coinbaseWallet({
-      appName: metadata.name,
-      appLogoUrl: metadata.icons[0],
-      headlessMode: true,
-    }),
+    // coinbaseWallet({
+    //   appName: metadata.name,
+    //   appLogoUrl: metadata.icons[0],
+    //   headlessMode: true,
+    // }),
   ],
 });
 
