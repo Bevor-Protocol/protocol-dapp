d674e827cec79efd7f34a8bc8bbe750926bc6be1
Peter Simone <plssimone@gmail.com>
2024-11-23 19:13:27 -0500

    add back ws

--- db/index.ts
+++ db/index.ts
@@ -1,5 +1,6 @@
-import { Pool } from "@neondatabase/serverless";
+import { neonConfig, Pool } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
+import ws from "ws";
 import { action } from "./schema/action.sql";
 import { auditMembership } from "./schema/audit-membership.sql";
 import { audit } from "./schema/audit.sql";
@@ -9,9 +10,9 @@ import * as relations from "./schema/relations.helpers";
 import { user } from "./schema/user.sql";
 import { wishlist } from "./schema/wishlist.sql";
 
-// if (process.env.NODE_ENV !== "production") {
-//   neonConfig.webSocketConstructor = ws;
-// }
+if (process.env.NODE_ENV !== "production") {
+  neonConfig.webSocketConstructor = ws;
+}
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL! });
 
